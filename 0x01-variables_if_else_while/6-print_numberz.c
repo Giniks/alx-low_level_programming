50156784a0ef7297bf47028d380df55927609078
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-04-24 10:26:48 -0700

    print number 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - single digits from base 10
+ * main - print single digits from 0 to base 10
  * Return: Always 0
  */
 int main(void)
 {
 	int i;
 	{
-		for (i = 0; i < 0; i++)
+		for (i = 0; i < 10; i++)
 		{
 			putchar(i + '0');
 		}
 	}
 
-	putchar("\n");
-	return (0);
+		putchat("\n");
+		return (0);
 }
