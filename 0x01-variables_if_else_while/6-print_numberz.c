2958d6cef667c3448782893a4534e65b8a0b9d65
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-04-24 10:16:32 -0700

    print numbers 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - single digits from base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	{
+		for (i = 0; i < 0; i++)
+		{
+			putchar(i + '0');
+		}
+	}
+
+	putchar("\n");
+	return (0);
+}
