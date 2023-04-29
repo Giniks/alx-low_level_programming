f064b31a3147de19dbee5be0c4bf76f060158644
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-04-28 17:42:14 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	long int div = 2;
+	long int largest_prime = 0;
+
+	while (num != 0)
+	{
+		if (num % div != 0)
+			div++;
+		else
+		{
+			largest_prime = num;
+			num = num / div;
+			if (num == 1)
+			{
+				printf("%ld\n", largest_prime);
+				break;
+			}
+		}
+	}
+	return (0);
+}
