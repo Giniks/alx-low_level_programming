a0765f3209fc059215bb3574f332e06d4264c0df
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-05 18:12:32 -0700

    task seven

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to encode.
+ *
+ * Return: The encoded string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char *letters = "aAeEoOtTlL";
+	char *nums = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+		}
+		str[i] = nums[j];
+		break;
+	}
+	return (str);
+}
