7f1c07632013c76facbf0d74623a6f5eb41823f5
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-05 20:48:28 -0700

    task three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,18 @@
  * _strcmp - compares two string values
  * @s1: string 1
  * @s2: string 2
- * Return: the diffrence in s1 and s2
+ * Return: 0 if string are equal, negstive if s1 is less than s2
+ * positive if s1 9s greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int 1;
+	int i = 0;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+			break;
 		i++;
 	}
-	return (0);
-}
+	return (s1[i] - s2[i]);
 }
