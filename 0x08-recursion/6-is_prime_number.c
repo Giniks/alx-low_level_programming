708e13397adbb29f5ce50bed0f20808814185008
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-10 20:16:37 -0700

    task six

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "msin.h"
+
+/**
+ * sqrt_helper - recursive helper function to calculate square root
+ *
+ * @n: the number to find the square root of
+ * @guess: the current guess for the square root
+ * Return: return the natural square root of n, -1 if it has no natura
+ * square root.
+ */
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - calculate the natural square root of a number
+ *
+ * @n: the numner to find square root of
+ *
+ * Return: the natural square root of n, -1 if it has no natural square
+ * root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_helper(n, 0));
+}
