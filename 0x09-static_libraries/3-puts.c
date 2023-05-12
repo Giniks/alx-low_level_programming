020cb7d34b98341cca228910502c061940c7eec9
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-12 15:00:14 -0700

    create_static_lib.sh

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+@include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @s: string of character to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
