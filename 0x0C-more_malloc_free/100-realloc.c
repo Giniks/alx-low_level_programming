6cfa7a1d070b7afddd2dc8a478d78730d2e006eb
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-05-19 06:05:27 -0700

    realloc - task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previouslt allocated
+ * @old_size: size in bytes of the allocated space of ptr
+ * @new_size: size in bytes of the memory block
+ * Return: ptr
+ * if new_size == old_size rwturn ptr without changes
+ * if malloc fails return NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if ((new_size == 0 && ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size == old_size)
+		return (ptr);
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
