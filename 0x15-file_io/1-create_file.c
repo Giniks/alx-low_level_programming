0e34717014438156b7bd5ec91d1fe3dc63404060
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-15 16:12:06 -0700

    create a file and add some text - task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	bytes_written = write(fd, text_content, length);
+
 	if (bytes_written == -1)
 		return (-1);
 	
