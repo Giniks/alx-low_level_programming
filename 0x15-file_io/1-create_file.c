0a60356cf271fc7dfa3784c5859812dd5b6aba9b
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-15 15:56:52 -0700

    Create and write a text content on a file - task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,16 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-		bytes_written = write(fd, text_content, length);
-
-	else
-		bytes_written = write(fd, "", 0);
-
-	close(fd);
-
 	if (bytes_written == -1)
 		return (-1);
-
-	return (-1);
+	
+	close(fd);
+	return (1);
 }
