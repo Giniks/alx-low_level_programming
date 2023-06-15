8ccc830af203074e6ad1ea5f0a7bb404b23d791c
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-15 16:36:45 -0700

    Create a file and add some text - task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,12 +19,18 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	bytes_written = write(fd, text_content, length);
 
-	if (bytes_written == -1)
+	if (bytes_written == -1 || bytes_written != length)
+	{
+		close(fd);
 		return (-1);
+	}
 	
 	close(fd);
 	return (1);
