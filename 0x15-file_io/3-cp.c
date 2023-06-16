e6fdf84fb6d39e4b6988954aa09ecb9c73cbecdf
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-16 10:40:57 -0700

    Copy file from_to - task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,18 +6,16 @@ char *create_buffer(char *file);
 void close_file(int fd);
 /**
  * create_buffer - Mapout 1024 bytes for a buffer
- * @fp: the name of the file buffer is storing character to
+ * @file: the name of the file buffer is storing character to
  *
  * Return: A pointer to the new allocated buffer
  */
 char *create_buffer(char *file)
 {
-	char *file;
-
-	buffer = malloc(sizeof(char) * 1024);
+	char *buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
 	{
-		if (buffer == 0)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fp);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 	return (buffer);
@@ -50,7 +48,7 @@ void close_file(int fd)
  * if file_to xannot be created or written to exit with code 99
  * if file_to or file_from cannot be closed exit with code 100
  */
-int main(char argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 	ssize_t byte_read, byte_written;
@@ -62,7 +60,7 @@ int main(char argc, char *argv[])
 		exit(97);
 	}
 	
-	buffer = create_buffer(argv[2]);
+	buffer = create_buffer(argv[1]);
 	file_from = open(argv[1], O_RDONLY);
 	byte_read = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -77,7 +75,7 @@ int main(char argc, char *argv[])
 		}
 		byte_written = write(file_to, buffer, byte_read);
 
-		if (file_to == -1 | byte_written == -1)
+		if (file_to == -1 || byte_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
 			free(buffer);
@@ -87,7 +85,7 @@ int main(char argc, char *argv[])
 		byte_read = read(file_from, buffer, 1024);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	while (bytes_read > 0);
+	while (byte_read > 0);
 
 	free(buffer);
 	close(file_from);
