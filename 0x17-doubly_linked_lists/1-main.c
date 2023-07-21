dbdc3e57b04b3e3a7fe7c8579ba06bc572c04dc2
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-20 19:40:23 -0700

    5-get_dnodeint

--- 0x17-doubly_linked_lists/1-main.c
+++ 0x17-doubly_linked_lists/1-main.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -28,7 +27,7 @@ int main(void)
 	new->next = head;
 	new->prev = NULL;
 	head = new;
-	n = print_dlistint(head);
+	n = dlistint_len(head);
 	printf("-> %lu elements\n", n);
 	free(new);
 	return (EXIT_SUCCESS);
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of the linked list
+ * @head: head of the list
+ * @index: index of node starting from 0
+ *
+ * Return: nth node or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *tmp;
+
+	size = 0
+		if (head == NULL)
+			return (NULL);
+
+	tmp = head;
+	while (tmp)
+	{
+		if (index == size)
+			return (tmp);
+		size++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
